677ffaec241d1621a03e95b8945ed5b8e419a6f1
jwebmodica <info@jwebmodica.it>
2026-02-10 12:59:12 +0100

    Aggiorna URL repository GitHub

--- logistika-woocommerce.php
+++ logistika-woocommerce.php
@@ -15,7 +15,7 @@
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
- * GitHub Plugin URI: https://github.com/logistika-dev/logistika-woocommerce
+ * GitHub Plugin URI: https://github.com/jwebmodica/logistika-woocommerce
  * GitHub Branch: main
  */
 
